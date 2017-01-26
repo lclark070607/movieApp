ad3f71989ec06bf029f2a0d36357e3d612c6d7e9
Louise Clark <lclark070607@gmail.com>
2017-01-26 15:24:35 -0500

    working the buttons

--- js/app.js
+++ js/app.js
@@ -2,18 +2,36 @@ const app = angular.module('MovieApp', []);
 
 app.controller('MovieListController', function ($scope, MovieService) {
     $scope.movies = MovieService.getMovies();
-    
 
-    $scope.newMovie = function (jamMovie) {
-        let jamMovie = [$scope.movieName, $scope.movieDate, $scope.movieGenre];
-        MovieService.addMovie($scope.movieName, $scope.movieDate, $scope.movieGenre);
-       //clears my textboxes after submit
+
+    $scope.newMovie = function () {
+        let jams = {
+            movieName: $scope.movieName,
+            movieDate: $scope.movieDate,
+            movieGenre: $scope.movieGenre,
+            movieLike: $scope.movieLike,
+            movieHate: $scope.movieHate,
+        };
+
+        MovieService.addMovie(jams);
+
+        //clears my textboxes after submit
         $scope.movieName = '';
         $scope.movieDate = '';
         $scope.movieGenre = '';
     };
 
+    $scope.movieLike = function (target) {
+        MovieService.likeMovie(target);
+    };
+
+    $scope.movieHate = function(zing) {
+        MovieService.hateMovie(zing);
+    };
+    
 });
+
+
 
 app.factory('MovieService', function () {
     let movies = [];
@@ -23,9 +41,21 @@ app.factory('MovieService', function () {
         addMovie: function (input) {
             movies.push(input);
         },
-        
+
+        bombMovie: function() {
+            movies.classList.add("dislike");
+        },
+
         getMovies: function () {
             return movies;
+        },
+
+        likeMovie: function(moo) {
+            moo.isLiked = true;
+        },
+
+        hateMovie: function(baa) {
+            baa.isHated = true;
         },
     };
 });

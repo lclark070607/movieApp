adf32376887b07e78d2c2ed144f9c3c10f72c105
Louise Clark <lclark070607@gmail.com>
2017-01-28 16:48:59 -0500

    working on stars transition

--- js/app.js
+++ js/app.js
@@ -7,8 +7,11 @@ function movie(title, release_date) {
     // this.movieGenre = movieGenre;
     this.rated = rated;
     this.poster_path = null;
+    this.overview = null;
     this.id = null;
     this.added = null;
+    this.isRated = false;
+    this.rating = null;
 
     return this;
 }
@@ -35,6 +38,10 @@ app.controller('NewMovieController', function ($scope, MovieService) {
 app.controller('ListMovieController', function ($scope, MovieService) {
     $scope.movies = MovieService.getMovies();
 
+    $scope.ratedMovie = function (current) {
+        MovieService.markAsRated(current);
+    };
+
 });
 
 
@@ -42,81 +49,86 @@ app.controller('StarRatingController', function ($scope, MovieService) {
     $scope.rating = 5;
 
     $scope.rateFunction = function (rating) {
-      alert('You selected - ' + rating+' stars');
+        alert('You selected - ' + rating + ' stars');
     };
-  })
+})
 
-  
-  .directive('starRating',
-            function () {
-                return {
-                    //String of subset of EACM which restricts the directive to a specific directive declaration style. If omitted, the defaults (elements and attributes) are used.
-                    restrict: 'A',
-                    template: '<ul class="rating">'
-                    + '	<li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">'
-                    + '  <i class="fa fa-star-o"></i>'
-                    //  + '\u2605'
-                    + '</li>'
-                    + '</ul>',
-                    scope: {
-                        ratingValue: '=',
-                        max: '=',
-                        onRatingSelected: '&'
-                    },
-                    link: function (scope, elem, attrs) {
-                        var updateStars = function () {
-                            scope.stars = [];
-                            for (var i = 0; i < scope.max; i++) {
-                                scope.stars.push({
-                                    filled: i < scope.ratingValue
-                                });
-                            }
-                        };
 
-                        scope.toggle = function (index) {
-                            scope.ratingValue = index + 1;
-                            scope.onRatingSelected({
-                                rating: index + 1
-                            });
-                        };
-
-                        scope.$watch('ratingValue',
-                            function (oldVal, newVal) {
-                                if (newVal) {
-                                    updateStars();
-                                }
-                            }
-                        );
+    .directive('starRating',
+    function () {
+        return {
+            //String of subset of EACM which restricts the directive to a specific directive declaration style. If omitted, the defaults (elements and attributes) are used.
+            restrict: 'A',
+            template: '<ul class="rating">'
+            + '	<li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">'
+            + '  <i class="fa fa-star-o"></i>'
+            //  + '\u2605'
+            + '</li>'
+            + '</ul>',
+            scope: {
+                ratingValue: '=',
+                max: '=',
+                onRatingSelected: '&'
+            },
+            link: function (scope, elem, attrs) {
+                var updateStars = function () {
+                    scope.stars = [];
+                    for (var i = 0; i < scope.max; i++) {
+                        scope.stars.push({
+                            filled: i < scope.ratingValue
+                        });
                     }
                 };
-            }
-        );
 
-
-        app.factory('MovieService', function ($http) {
-            let movies = [];
-
-            $http.get('https://api.themoviedb.org/4/list/14842?page=1&api_key=b2ff2aa190e51a05f0b622f779200cd5').then(function (response) {
-                angular.copy(response.data.results, movies);
-            });
-
-            return {
-
-                add(movie) {
-                    movies.push(movie);
-
-                    $http.post('https://api.themoviedb.org/4/list/14842?page=1&api_key=b2ff2aa190e51a05f0b622f779200cd5', {
-                        title: movie.title,
-                        release_date: movie.release_date,
+                scope.toggle = function (index) {
+                    scope.ratingValue = index + 1;
+                    scope.onRatingSelected({
+                        rating: index + 1
                     });
-                },
+                };
 
-                getMovies: function () {
-                    return movies;
-                },
+                scope.$watch('ratingValue',
+                    function (oldVal, newVal) {
+                        if (newVal) {
+                            updateStars();
+                        }
+                    }
+                );
+            }
+        };
+    }
+    );
 
-            };
-        });
+
+app.factory('MovieService', function ($http) {
+    let movies = [];
+
+    $http.get('https://api.themoviedb.org/4/list/14842?page=1&api_key=b2ff2aa190e51a05f0b622f779200cd5').then(function (response) {
+
+        angular.copy(response.data.results, movies);
+    });
+
+    return {
+
+        add(movie) {
+            movies.push(movie);
+
+            $http.post('https://api.themoviedb.org/4/list/14842?page=1&api_key=b2ff2aa190e51a05f0b622f779200cd5', {
+                title: movie.title,
+                release_date: movie.release_date,
+            });
+        },
+
+        getMovies: function () {
+            return movies;
+        },
+
+        markAsRated(stars) {
+            stars.isRated = true;
+        },
+
+    };
+});
 
 
 
